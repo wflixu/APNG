66f178ab635f67125c0ac9802701bacbaffd44f8
wflixu <wflixu@88.com>
2025-06-22 20:51:18 +0800

    Initial Commit

---
+++ APNG/APNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  APNGApp.swift
+//  APNG
+//
+//  Created by 李旭 on 2025/6/22.
+//
+
+import SwiftUI
+
+@main
+struct APNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APNG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APNG
+//
+//  Created by 李旭 on 2025/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
